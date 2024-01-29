0bbbf6e7de50bb07232a3a216adad99e65f0c5cf
simobls <belassalsimo78@gmail.com>
2024-01-29 19:27:05 +0100

    add

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -2,6 +2,8 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+    if (!parent)
+        return (NULL);
 
     binary_tree_t *new = binary_tree_node(parent, value);
 
