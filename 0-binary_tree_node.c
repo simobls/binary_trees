5a279ef2a4ce13c003f83b31c13901bd0bb726c3
simobls <belassalsimo78@gmail.com>
2024-01-29 19:10:47 +0100

    add

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new;
+
+    new = malloc(sizeof(binary_tree_t));
+    if (new)
+    {
+        new->n = value;
+        new->parent = parent;
+        new->left = NULL;
+        new->right = NULL;
+        return (new);
+    }
+    return (NULL);
+}
